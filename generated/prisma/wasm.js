cf55c34d2188ecca10dc1ed690b8a3dff2c0e9eb
mciyer <mciyer@gmail.com>
2025-11-20 12:18:59 -0500

    Ani's code review
    I am goated (ani)

--- generated/prisma/wasm.js
+++ generated/prisma/wasm.js
@@ -207,7 +207,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "/Users/txi/Documents/GitHub/findmaxxing/generated/prisma",
+      "value": "/Users/Mark/Documents/GitHub/findmaxxing/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -221,7 +221,7 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "/Users/txi/Documents/GitHub/findmaxxing/prisma/schema.prisma",
+    "sourceFilePath": "/Users/Mark/Documents/GitHub/findmaxxing/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
